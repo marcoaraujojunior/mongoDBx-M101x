63b02d7562dd97cbbe3e276a61d95561575c1f20
Marco Araujo <marco.araujo.junior@gmail.com>
2016-09-02 09:41:26 -0300

    Refactoring

--- app/server/index.js
+++ app/server/index.js
@@ -14,5 +14,7 @@ wagner.invoke(rootRequire('services/auth'), { app: app });
 
 app.use('/api/v1', rootRequire('api/v1/api')(wagner));
 
+app.use(express.static('../', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
+
 app.listen(3000);
 console.log('Listening on port 3000!');
